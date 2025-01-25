a9cda74d78b111a589f71ac5164a0495dfc165f5
@Always <zhxy_chn@126.com>
2025-01-25 09:47:39 +0800

    其他修改：pnpm format 格式化
    1.原因分析：无
    2.修改方法：无

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -1,21 +1,21 @@
 /*
-* 在 Hexo 根目录下的 source/_data 文件夹中（没有的文件夹需要自己创建），新建一个 Yaml 配置文件，并命名为主题的名称，
-* 例如 Stun 主题，就叫做 stun.yml。然后将主题配置文件 _config.yml 里的内容复制到 stun.yml 中，这样 stun.yml
-* 就会覆盖 _config.yml。如果想要修改配置，直接修改 stun.yml 就好了。当用户更新主题或者开发者发布主题时，
-* 配置数据保存在 stun.yml 中，主题原来的配置文件 _config.yml 仍然是干净的。
-* */
+ * 在 Hexo 根目录下的 source/_data 文件夹中（没有的文件夹需要自己创建），新建一个 Yaml 配置文件，并命名为主题的名称，
+ * 例如 Stun 主题，就叫做 stun.yml。然后将主题配置文件 _config.yml 里的内容复制到 stun.yml 中，这样 stun.yml
+ * 就会覆盖 _config.yml。如果想要修改配置，直接修改 stun.yml 就好了。当用户更新主题或者开发者发布主题时，
+ * 配置数据保存在 stun.yml 中，主题原来的配置文件 _config.yml 仍然是干净的。
+ * */
 hexo.on('generateBefore', function () {
-    var rootConfig = hexo.config;
+  var rootConfig = hexo.config;
 
-    if (hexo.locals.get) {
-        // 获取 source/_data 目录下的文件
-        var data = hexo.locals.get('data');
+  if (hexo.locals.get) {
+    // 获取 source/_data 目录下的文件
+    var data = hexo.locals.get('data');
 
-        // 如果存在 anatolo_config.yml 文件，就用它覆盖原来的配置文件
-        if (data && data.anatolo_config) {
-            hexo.theme.config = data.anatolo_config;
-        }
+    // 如果存在 anatolo_config.yml 文件，就用它覆盖原来的配置文件
+    if (data && data.anatolo_config) {
+      hexo.theme.config = data.anatolo_config;
     }
+  }
 
-    hexo.theme.config.rootConfig = rootConfig;
+  hexo.theme.config.rootConfig = rootConfig;
 });
